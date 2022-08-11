d00e3e463c742399a3d0258c9dbb141a5cefec71
Harsha <konndapuramharsha@gmail.com>
2022-08-11 21:18:19 +0530

    express response error fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 var cors = require('cors');
 const express = require('express');
 const dotenv = require('dotenv');
-const fetch = require('node-fetch');
 var cloudinary = require('cloudinary');
 dotenv.config();
 const app = express();
@@ -12,11 +11,9 @@ const router = express.Router();
 const port = process.env.PORT||9000;
 
 
-
-
-
-
 const  fetchImagesInCloudinary = async  (req,res)=> {
+    try {
+  
   await  cloudinary.config({ 
         cloud_name:process.env.CLOUD_NAME, 
         api_key:process.env.API_KEY, 
@@ -27,13 +24,23 @@ const  fetchImagesInCloudinary = async  (req,res)=> {
   var response =   await  cloudinary.v2.api.resources(
         function(error, result) {console.log(result, error); });
     if(response){
-        return res.status(200).json(response);
+        return response;
     }    
+          
+} catch (error) {
+       return console.log(error)
+}
 
 }
-fetchImagesInCloudinary()
-router.get('/getAllImages',fetchImagesInCloudinary);
+const getAllImages =  async(req,res)=>{
+const response = await fetchImagesInCloudinary();
+console.log(response)
+return res.status(200).json(response);
+}
+router.get('/getAllImages',getAllImages);
 app.use(router);
+
+
 app.listen(port,() => {
     console.log("Server is  running up and healthy");
   });
